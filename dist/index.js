b41e6b8ee9f0ceb34e533a59e0b99fe45f5dacf8
dingtalk_xsgrpl <1134954328@qq.com>
2022-09-21 21:31:47 +0800

    通过 eneity 创建数据

--- dist/index.js
+++ dist/index.js
@@ -8,23 +8,37 @@ var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/
 
 var _dataSource = require("./data-source");
 
+var _Post = require("./entity/Post");
+
 _dataSource.AppDataSource.initialize().then( /*#__PURE__*/(0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee() {
+  var posts, p, posts2;
   return _regenerator["default"].wrap(function _callee$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
-          console.log("Inserting a new user into the database..."); // const user = new User()
-          // user.firstName = "Timber"
-          // user.lastName = "Saw"
-          // user.age = 25
-          // await AppDataSource.manager.save(user)
-          // console.log("Saved a new user with id: " + user.id)
-          // console.log("Loading users from the database...")
-          // const users = await AppDataSource.manager.find(User)
-          // console.log("Loaded users: ", users)
-          // console.log("Here you can setup and run express / fastify / any other framework.")
+          _context.next = 2;
+          return _dataSource.AppDataSource.manager.find(_Post.Post);
 
-        case 1:
+        case 2:
+          posts = _context.sent;
+          console.log("开始");
+          console.log(posts);
+          p = new _Post.Post();
+          p.title = "Post 1";
+          p.content = "我的第一篇文章";
+          _context.next = 10;
+          return _dataSource.AppDataSource.manager.save(p);
+
+        case 10:
+          _context.next = 12;
+          return _dataSource.AppDataSource.manager.find(_Post.Post);
+
+        case 12:
+          posts2 = _context.sent;
+          console.log("结束");
+          console.log(posts2);
+
+        case 15:
         case "end":
           return _context.stop();
       }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import { AppDataSource } from "./data-source"
+import { Post } from "./entity/Post"
 
 AppDataSource.initialize()
   .then(async () => {
-    console.log("Inserting a new user into the database...")
-    // const user = new User()
-    // user.firstName = "Timber"
-    // user.lastName = "Saw"
-    // user.age = 25
-    // await AppDataSource.manager.save(user)
-    // console.log("Saved a new user with id: " + user.id)
-
-    // console.log("Loading users from the database...")
-    // const users = await AppDataSource.manager.find(User)
-    // console.log("Loaded users: ", users)
-
-    // console.log("Here you can setup and run express / fastify / any other framework.")
+    const posts = await AppDataSource.manager.find(Post)
+    console.log("开始")
+    console.log(posts)
+    const p = new Post()
+    p.title = "Post 1"
+    p.content = "我的第一篇文章"
+    await AppDataSource.manager.save(p)
+    const posts2 = await AppDataSource.manager.find(Post)
+    console.log("结束")
+    console.log(posts2)
   })
   .catch(error => console.log(error))
