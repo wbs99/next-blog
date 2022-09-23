84e4f9facbc1d7e870c31edf89ea6a77ddde15a7
dingtalk_xsgrpl <1134954328@qq.com>
2022-09-23 20:24:41 +0800

    成功实现注册，暂未检测 user 唯一性

---
+++ pages/api/v1/users.tsx
@@ -0,0 +1,50 @@
+import { getDataBaseConnection } from "lib/getDataBaseConnection"
+import { NextApiHandler } from "next"
+import { User } from "src/entity/User"
+import md5 from "md5"
+
+const Users: NextApiHandler = async (req, res) => {
+  const { username, password, passwordConfirmation } = req.body
+
+  const errors = {
+    username: [] as string[],
+    password: [] as string[],
+    passwordConfirmation: [] as string[],
+  }
+  if (username.trim() === "") {
+    errors.username.push("用户名不能为空")
+  }
+  if (!/[a-zA-Z0-9]/.test(username.trim())) {
+    errors.username.push("格式不合法")
+  }
+  if (username.trim().length > 42) {
+    errors.username.push("用户名太长")
+  }
+  if (username.trim().length <= 3) {
+    errors.username.push("用户名太短")
+  }
+  if (password === "") {
+    errors.password.push("密码不能为空")
+  }
+  if (password !== passwordConfirmation) {
+    errors.password.push("密码不匹配")
+  }
+
+  const hasError = Object.values(errors).find(item => item.length > 0)
+  res.setHeader("Content-Type", "application/json;charset=utf-8")
+  if (hasError) {
+    res.statusCode = 422
+    res.write(JSON.stringify(errors))
+  } else {
+    const connection = await getDataBaseConnection()
+    const user = new User()
+    user.username = username.trim()
+    user.passwordDigest = md5(password)
+    await connection.manager.save(user)
+    res.statusCode = 200
+    res.write(JSON.stringify(user))
+  }
+  res.end()
+}
+
+export default Users
---
+++ pages/sign_up.tsx
@@ -0,0 +1,98 @@
+import axios, { AxiosResponse } from "axios"
+import { NextPage } from "next"
+import { useCallback, useState } from "react"
+
+const SignUp: NextPage = props => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    passwordConfirmation: "",
+  })
+  const [errors, setErrors] = useState({
+    username: [],
+    password: [],
+    passwordConfirmation: [],
+  })
+
+  const onSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      axios.post(`api/v1/users`, formData).then(
+        () => {},
+        error => {
+          if (error.response) {
+            const response: AxiosResponse = error.response
+            if (response.status === 422) {
+              setErrors({ ...error, ...response.data })
+            }
+          }
+        }
+      )
+    },
+    [formData]
+  )
+  return (
+    <>
+      <h1>注册</h1>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label>
+            用户名
+            <input
+              type="text"
+              value={formData.username}
+              onChange={e => {
+                setFormData({
+                  ...formData,
+                  username: e.target.value,
+                })
+              }}
+            />
+          </label>
+          {errors.username?.length > 0 && (
+            <div>{errors.username.join(",")}</div>
+          )}
+        </div>
+        <div>
+          <label>
+            密码
+            <input
+              type="password"
+              value={formData.password}
+              onChange={e => {
+                setFormData({
+                  ...formData,
+                  password: e.target.value,
+                })
+              }}
+            />
+          </label>
+          {errors.password?.length > 0 && (
+            <div>{errors.password.join(",")}</div>
+          )}
+        </div>
+        <div>
+          <label>
+            确认密码
+            <input
+              type="password"
+              value={formData.passwordConfirmation}
+              onChange={e => {
+                setFormData({
+                  ...formData,
+                  passwordConfirmation: e.target.value,
+                })
+              }}
+            />
+          </label>
+          {errors.passwordConfirmation?.length > 0 && (
+            <div>{errors.passwordConfirmation.join(",")}</div>
+          )}
+        </div>
+        <button type="submit">注册</button>
+      </form>
+    </>
+  )
+}
+
+export default SignUp
