c21590b82a6e8920e04f6d0da778bfa8f4320489
dingtalk_xsgrpl <1134954328@qq.com>
2022-09-25 20:34:02 +0800

    优化首页和文章列表的页的代码复用

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,4 @@
-import { GetServerSideProps, NextPage } from "next"
-import { Post } from "src/entity/Post"
-import { getDataBaseConnection } from "lib/getDataBaseConnection"
-import Link from "next/link"
+import PostsIndex, { getStaticProps } from "./posts"
 
-type Props = {
-  posts: Post[]
-}
-
-const index: NextPage<Props> = props => {
-  const { posts } = props
-  return (
-    <div>
-      <h1>文章列表</h1>
-      {posts.map(post => (
-        <Link key={post.id} href={`/posts/${post.id}`}>
-          <a>{post.title}</a>
-        </Link>
-      ))}
-    </div>
-  )
-}
-export default index
-
-export const getServerSideProps: GetServerSideProps = async context => {
-  const connection = await getDataBaseConnection()
-  const posts = await connection.manager.find(Post)
-
-  return {
-    props: {
-      posts: JSON.parse(JSON.stringify(posts)),
-    },
-  }
-}
+export default PostsIndex
+export { getStaticProps }
